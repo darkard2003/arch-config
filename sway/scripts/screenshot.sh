78aa7da4f4ed46bf8f903e5c3ce043adc1b4a789
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2025-01-11 17:47:40 +0530

    Improved screenshot in sway

--- sway/scripts/screenshot.sh
+++ sway/scripts/screenshot.sh
@@ -8,27 +8,52 @@ openScreenshot(){
     kitty --app-id="user-dialog" --config="/dev/null" -e bash -c "kitten icat '$1'; read -p 'press enter to close...'" 
 }
 
+saveScreenshot(){
+  local filename="$1"
+  local filepath="$2"
+
+  local save_dir="${PICTURES_DIR}/Screenshot"
+
+  if [ ! -d $save_dir ]; then
+    mkdir -p $save_dir
+  fi
+
+
+  local savepath="${PICTURES_DIR}/Screenshot/$filename"
+  mv "$filepath" "$savepath"
+
+  notify-send "Screenshot" "Screenshot saved to $savepath"
+  
+}
+
 notify_and_open() {
-    local filepath="$1"
-    wl-copy < "$filepath"
-    action=$(notify-send -A "open=Open" "Screenshot" "Screenshot saved to $filepath")
+    local filename="$1"
+    local tmp_path="$2"
+
+    wl-copy < "$tmp_path"
+
+    action=$(notify-send -A "open=Open" -A "save=Save" "Screenshot" "Screenshot $filename taken")
+
     if [ "$action" = "open" ]; then
-        openScreenshot "$filepath"
+        openScreenshot "$filename"
+    elif [ "$action" = "save" ]; then
+      saveScreenshot "$filename" "$tmp_path"
     fi
 }
 
 take_screenshot() {
     local mode="$1"
-    local filepath="${PICTURES_DIR}/$(date -Iseconds)_grim.png"
+    local filename="$(date -Iseconds)_grim.png"
+    local tmp_path="/tmp/$filename"
     
     if [ "$mode" = "full" ]; then
-        grim "$filepath"
+        grim "$tmp_path"
     else
-        grim -g "$(slurp)" "$filepath"
+        grim -g "$(slurp)" "$tmp_path"
     fi
 
-    if [ -f "$filepath" ]; then
-        notify_and_open "$filepath"
+    if [ -f "$tmp_path" ]; then
+        notify_and_open "$filename" "$tmp_path"
     else
         notify-send "Screenshot failed"
         return 1
