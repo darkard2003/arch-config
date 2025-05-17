64c2ecfa5f2d9470f4a7338191b89448087597dd
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2025-05-17 19:28:05 +0530

    Fix sddm setup

--- setup_kanata.sh
+++ setup_kanata.sh
@@ -1,19 +1,34 @@
 echo "Give sudo permission to continue"
 sudo -v
 
+TEMP_DIR="./temp"
+
+if [ ! -d $TEMP_DIR ]; then
+  mkdir $TEMP_DIR
+fi
+
 sudo groupadd uinput
 
 sudo usermod -aG uinput $USER
 sudo usermod -aG input $USER
 
-touch /etc/udev/rules.d/99-input.rules
+# touch /etc/udev/rules.d/99-input.rules
+touch $TEMP_DIR/99-input.rules
 
-sudo echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' > /etc/udev/rules.d/99-input.rules
+echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' > $TEMP_DIR/99-input.rules
+sudo cp $TEMP_DIR/99-input.rules /etc/udev/rules.d/99-input.rules
+
 sudo udevadm control --reload-rules && sudo udevadm trigger
 
 sudo modprobe uinput
 
-cp -sf "./services/kanata.service" "$HOME/.config/systemd/user/kanata.service"
-sudo systemctl --user enable kanata.service
+CONFIG_DIR="$HOME/.config/systemd/user"
+
+if [ ! -d $CONFIG_DIR ]; then 
+  mkdir -p $CONFIG_DIR
+fi
+
+cp "./services/kanata.service" "$HOME/.config/systemd/user/kanata.service"
+systemctl --user enable kanata.service
 
 echo "Reboot your system to take effect"
--- setup_sddm.sh
+++ setup_sddm.sh
@@ -1,4 +1,4 @@
-TEMP_DIR="temp"
+TEMP_DIR="./temp"
 
 if [ ! -d "$TEMP_DIR" ]; then
     mkdir $TEMP_DIR
@@ -10,10 +10,9 @@ CATPPUCCIN_THEME_NAME="where-is-my-sddm-theme-catppuccin"
 CATPPUCCIN_THEME_URL="https://github.com/catppuccin/where-is-my-sddm-theme.git"
 SDDM_THEME_DIR="/usr/share/sddm/themes"
 
-git clone "$CATPPUCCIN_THEME_URL" "$TEMP_DIR/$CATPPUCCIN_THEME_NAME"
-git clone "$THEME_URL" "$TEMP_DIR/$THEME_NAME"
+[ -d "$TEMP_DIR/$CATPPUCCIN_THEME_NAME" ] || git clone "$CATPPUCCIN_THEME_URL" "$TEMP_DIR/$CATPPUCCIN_THEME_NAME"
+[ -d "$TEMP_DIR/$THEME_NAME" ] || git clone "$THEME_URL" "$TEMP_DIR/$THEME_NAME"
 
 sudo -v
-sudo mkdir -p "$SDDM_THEME_DIR/$THEME_NAME"
-sudo cp "$TEMP_DIR/$THEME_NAME/where_is_my_sddm_theme/*"  "$SDDM_THEME_DIR/$THEME_NAME" -r
-sudo cp "$TEMP_DIR/$CATPPUCCIN_THEME_NAME/themes/catppuccin-mocha.conf" "$SDDM_THEME_DIR/$THEME_NAME/theme.conf"
+[ -d "$SDDM_THEME_DIR/$THEME_NAME" ] || sudo mkdir -p "$SDDM_THEME_DIR/$THEME_NAME"
+sudo cp "$TEMP_DIR/$THEME_NAME/where_is_my_sddm_theme/"  "$SDDM_THEME_DIR/$THEME_NAME" -r
