2db58ff1245db729f9d6ecc953590f44e6d54b0a
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2025-05-17 23:34:08 +0530

    feat: Added quick access terminnal

--- setup_grub.sh
+++ setup_grub.sh
@@ -6,7 +6,5 @@ wd="$PWD"
 
 TEMP_DIR_GRUB="$TEMP_DIR/$GRUB_DIR"
 
-git clone https://github.com/catppuccin/grub.git $TEMP_DIR_GRUB && cd $TEMP_DIR_GRUB
-sudo cp -r src/* /usr/share/grub/themes/
-
-cd $wd
+git clone https://github.com/catppuccin/grub.git $TEMP_DIR_GRUB
+sudo cp -r $TEMP_DIR_GRUB/src/* /usr/share/grub/themes/
