92d25462b94db7d04731d855a9fd9485c49b803e
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2026-05-18 14:04:39 +0530

    feat: Fixed treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,50 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		require'nvim-treesitter.configs'.setup {
-		  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-		  sync_install = false,
-		  auto_install = true,
-		  ignore_install = { "javascript" },
-		  highlight = {
-		    enable = true,
-		    disable = function(lang, buf)
-			local max_filesize = 100 * 1024 -- 100 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-			    return true
-			end
-		    end,
-		    additional_vim_regex_highlighting = false,
-		  },
-		}
-	end
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  build = ":TSUpdate",
+  config = function()
+    local ts = require("nvim-treesitter")
+    local langs = {
+      "go",
+      "zig",
+      "c",
+    }
+    ts.install(langs)
 
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*", -- Trigger on ALL filetypes
+      callback = function(args)
+        local bufnr = args.buf
+        local ft = vim.bo[bufnr].filetype
+        local ignore_fts = {
+          "TelescopePrompt",
+          "TelescopeResults",
+          "cmp_menu",
+          "cmp_docs",
+          "lazy",
+          "mason",
+          "NvimTree",
+          "neo-tree",
+          "notify",
+          "prompt",
+        }
+
+        if vim.tbl_contains(ignore_fts, ft) then
+          return
+        end
+
+        local lang = vim.treesitter.language.get_lang(ft)
+
+        if not lang then
+          return
+        end
+
+        local has_parser = pcall(vim.treesitter.language.add, lang)
+
+        if has_parser then
+          vim.treesitter.start(bufnr, lang)
+        end
+      end,
+    })
+  end,
 }
