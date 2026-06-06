1cb7da8a111bad2e2bb07e663c46cb77a9414db8
@darkard2003 <kaushikchowdhury231@gmail.com>
2026-06-06 20:37:08 +0530

    feat: Changed wallpaper

--- nvim/lua/plugins/cursortab.lua
+++ nvim/lua/plugins/cursortab.lua
@@ -1,5 +1,6 @@
 return {
   "cursortab/cursortab.nvim",
+  keys = "<C-Tab>",
   build = "cd server && go build",
   config = function()
     require("cursortab").setup({
@@ -7,6 +8,10 @@ return {
         type = "sweep",
         url = "http://darkmac:11434",
         model = "sweepai/sweep-next-edit",
+        context_size = 1024,
+        max_tokens = 64,
+        top_k = 50,
+        max_diff_history_tokens = 64,
       },
       keymaps = {
         accept = "<Tab>",
